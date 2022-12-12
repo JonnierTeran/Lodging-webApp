05eab5874d89c0eaec5d86b13ad53821d86c3dd5
Jonnier Andres Teran Morales <jonnierteranmorales@gmail.com>
2022-12-12 12:51:32 -0500

    WebApp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlojamientosComponent } from './Components/alojamientos/alojamientos.component';
+import { CreateComponent } from './Components/create/create.component';
+import { HomeComponent } from './Components/home/home.component';
+import { HouseComponent } from './Components/house/house.component';
+import { LoginComponent } from './Components/login/login.component';
+import { OfertarComponent } from './Components/ofertar/ofertar.component';
+import { RegistroComponent } from './Components/registro/registro.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:"" , component: LoginComponent},
+  {path:"Home" , component:HomeComponent},
+  {path:"Login" , component:LoginComponent},
+  {path:"Registrar" , component:RegistroComponent},
+  {path:"House" , component:HouseComponent},
+  {path:"Alojamiento" , component:AlojamientosComponent},
+  {path:"create" , component: CreateComponent},
+  {path:"Ofertar" , component:OfertarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Components/login/login.component';
+import { HomeComponent } from './Components/home/home.component';
+import { RegistroComponent } from './Components/registro/registro.component';
+import { CreateComponent } from './Components/create/create.component';
+import { OfertarComponent } from './Components/ofertar/ofertar.component';
+import { HouseComponent } from './Components/house/house.component';
+import { AlojamientosComponent } from './Components/alojamientos/alojamientos.component';
+import { InicioSesionComponent } from './Components/inicio-sesion/inicio-sesion.component';
+
+import { DataService } from './Services/data.service'
+
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    RegistroComponent,
+    CreateComponent,
+    OfertarComponent,
+    HouseComponent,
+    AlojamientosComponent,
+    InicioSesionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
