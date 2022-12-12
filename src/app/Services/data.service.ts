ae5a1e2b85bc999d8cff88677dcaf1d65add7de1
Jonnier Andres Teran Morales <jonnierteranmorales@gmail.com>
2022-12-12 14:36:57 -0500

    Service

--- src/app/Services/data.service.ts
+++ src/app/Services/data.service.ts
@@ -1,14 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+    Url:string;
 
-  constructor(private _http:HttpClient ) { }
 
-  GetUser(){
-    this._http.get()
+  constructor(private _http:HttpClient ) { 
+    this.Url = 'https://room-serve.onrender.com/'
+  }
+
+  public GetUser():Observable<any>{
+    return this._http.get(this.Url+'api/'+'users')
   }
 }
